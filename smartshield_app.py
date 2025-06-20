7ba8af4b3217b4622face13cab021bf98ee06e85
01chinnu <155171126+01chinnu@users.noreply.github.com>
2025-06-20 20:57:21 +0530

    Update smartshield_app.py

--- smartshield_app.py
+++ smartshield_app.py
@@ -124,7 +124,7 @@ authenticator = stauth.Authenticate(
     config["cookie"]["expiry_days"]
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login(location="main")
 
 # --- MAIN APP ---
 if authentication_status:
